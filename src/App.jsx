341dd61070dd8533ac59fcf03c4eada6e6d8b458
suryakiran18239 <92254802+suryakiran18239@users.noreply.github.com>
2025-06-22 17:19:29 +0000

    header and mobile stylings added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <Navbar />
+    <div className="w-full overflow-hidden">
+      <Header />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets.js";
 
 function Navbar() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
   return (
     <div className="absolute top-0 left-0 w-full z-10">
-      <div>
+      <div className="container flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
         <img src={assets.logo} alt="" />
-        <ul>
+        <ul className="hidden md:flex gap-7 text-white">
           <a href="#Header" className="cursor-pointer hover:text-gray-400">
             Home
           </a>
@@ -23,6 +25,41 @@ function Navbar() {
         <button className="hidden md:block bg-white px-8 py-2 rounded-full">
           Sign up
         </button>
+        <img
+          src={assets.menu_icon}
+          alt=""
+          className="md:hidden w-7 cursor-pointer"
+          onClick={() => setShowMobileMenu(true)}
+        />
+      </div>
+      {/* ------------------- mobile menu ------------------- */}
+      <div
+        className={`md:hidden ${
+          showMobileMenu ? "fixed w-full" : "w-0 h-0"
+        }  right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}
+      >
+        <div className="flex justify-end p-6 cursor=pointer">
+          <img
+            onClick={() => setShowMobileMenu(false)}
+            src={assets.cross_icon}
+            className="w-6"
+            alt=""
+          />
+        </div>
+        <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+          <a href="#Header" className="px-4 py-2 rounded-full inline-block">
+            Home
+          </a>
+          <a href="#Header" className="px-4 py-2 rounded-full inline-block">
+            About
+          </a>
+          <a href="#Header" className="px-4 py-2 rounded-full inline-block">
+            Projects
+          </a>
+          <a href="#Header" className="px-4 py-2 rounded-full inline-block">
+            Testimonials
+          </a>
+        </ul>
       </div>
     </div>
   );
