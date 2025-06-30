22942d7eaa5280a4c4f456cca15daf5a574cb661
suryakiran18239 <92254802+suryakiran18239@users.noreply.github.com>
2025-06-30 16:19:41 +0000

    added title, price, location to projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -25,11 +25,25 @@ const Projects = () => {
         </button>
       </div>
       {/* Project slider container */}
-      <div>
-        <div>
+      <div className="overflow-hidden">
+        <div className="flex gap-8 transition-transform duration-500 ease-in-out">
           {projectsData.map((project, index) => (
-            <div key={index}>
-              <img src={project.image} alt={project.title} />
+            <div key={index} className="relative flex-shrink-0 w-full sm:w-1/4">
+              <img
+                src={project.image}
+                alt={project.title}
+                className="w-full h-auto mb-14"
+              />
+              <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md">
+                  <h2 className="text-xl font-semibold text-gray-800">
+                    {project.title}
+                  </h2>
+                  <p className="text-gray-500 text-sm">
+                    {project.price} <span>|</span> {project.location}
+                  </p>
+                </div>
+              </div>
             </div>
           ))}
         </div>
