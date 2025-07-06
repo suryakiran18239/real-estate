f73a681ad6223dd1c949d2ba9475255efe71930b
suryakiran18239 <92254802+suryakiran18239@users.noreply.github.com>
2025-07-06 07:55:17 +0000

    Installed react-toastify

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
       <Testimonials />
+      <Contact />
     </div>
   );
 }
